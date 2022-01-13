eaf248594d73062a438cd53f6b21209ea4a88fb6
Somdotta07 <jhilick.s@gmail.com>
2022-01-13 09:52:56 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -43,3 +43,31 @@ ADD CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id);
 --Add column owner_id which is a foreign key referencing the owners table
 ALTER TABLE animals ADD COLUMN owner_id INT,
 ADD CONSTRAINT fk_owners FOREIGN KEY(owner_id) REFERENCES owners(id);
+
+------------------------DAY 4----------------------------------
+--Create a table named vets 
+CREATE TABLE vets(
+   id SERIAL PRIMARY KEY,
+   name VARCHAR(255),
+   age INT,
+   date_of_graduation DATE
+   );
+--There is a many-to-many relationship between the tables species and vets: a vet can specialize in multiple species, and a species can have multiple vets specialized in it.
+-- Create a "join table" called specializations to handle this relationship.
+CREATE TABLE specializations (
+   species_id INT,
+   vets_id INT,
+   FOREIGN KEY(species_id) REFERENCES species(id),
+   FOREIGN KEY(vets_id) REFERENCES vets(id),
+   UNIQUE(species_id,vets_id)
+   );
+--There is a many-to-many relationship between the tables animals and vets: an animal can visit multiple vets and one vet can be visited by multiple animals.
+-- Create a "join table" called visits to handle this relationship, it should also keep track of the date of the visit.
+CREATE TABLE visits (
+   animals_id INT,
+   vets_id INT,
+   date_of_visit timestamp,
+   FOREIGN KEY(animals_id) REFERENCES species(id),
+   FOREIGN KEY(vets_id) REFERENCES vets(id),
+   UNIQUE(animals_id,vets_id)
+   );
