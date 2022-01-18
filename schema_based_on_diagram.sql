2df2ad4e5439af205bd626feab374cb1d7999c9d
Shahier Nashaat <shahiern@yahoo.com>
2022-01-18 15:51:14 +0200

    Fix errors

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,11 +1,11 @@
 CREATE TABLE patients (
-    id INT SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(220),
     date_of_birth DATE
 );
 
 CREATE TABLE medical_histories (
-    id INT SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     admitted_at TIMESTAMP,
    patient_id INT,
    status VARCHAR(220),
@@ -13,9 +13,9 @@ CREATE TABLE medical_histories (
 );
 
 CREATE TABLE treaments (
-    id INT SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     type VARCHAR(220),
-    name VARCHAR(220),
+    name VARCHAR(220)
 );
 
 CREATE TABLE medical_histories_treaments (
@@ -26,7 +26,7 @@ CREATE TABLE medical_histories_treaments (
 );
 
 CREATE TABLE invoices (
-    id INT SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     total_amount DECIMAL,
     generated_at TIMESTAMP,
     payed_at TIMESTAMP,
@@ -35,7 +35,7 @@ CREATE TABLE invoices (
 );
 
 CREATE TABLE invoice_items (
-    id INT SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     unit_price DECIMAL,
     quantity INT,
     total_price DECIMAL,
