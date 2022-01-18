9c810e33abb02d245e0f79892283575b8cd24dce
unknown <okpara_tochukwu@yahoo.com>
2022-01-18 14:31:39 +0100

    update invoices and invoice_items

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,11 +1,11 @@
 CREATE TABLE patients (
-    id SERIAL PRIMARY KEY,
+    id INT SERIAL PRIMARY KEY,
     name VARCHAR(220),
     date_of_birth DATE
 );
 
 CREATE TABLE medical_histories (
-    id SERIAL PRIMARY KEY,
+    id INT SERIAL PRIMARY KEY,
     admitted_at TIMESTAMP,
    patient_id INT,
    status VARCHAR(220),
@@ -13,7 +13,7 @@ CREATE TABLE medical_histories (
 );
 
 CREATE TABLE treaments (
-    id SERIAL PRIMARY KEY,
+    id INT SERIAL PRIMARY KEY,
     type VARCHAR(220),
     name VARCHAR(220),
 );
@@ -22,5 +22,25 @@ CREATE TABLE medical_histories_treaments (
     medical_history_id int,
     treament_id int,
     CONSTRAINT fk_medical_history FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id),
+    CONSTRAINT fk_treament FOREIGN KEY(treament_id) REFERENCES treaments(id)
+);
+
+CREATE TABLE invoices (
+    id INT SERIAL PRIMARY KEY,
+    total_amount DECIMAL,
+    generated_at TIMESTAMP,
+    payed_at TIMESTAMP,
+    medical_history_id INT,
+    CONSTRAINT fk_medical_history FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id)
+);
+
+CREATE TABLE invoice_items (
+    id INT SERIAL PRIMARY KEY,
+    unit_price DECIMAL,
+    quantity INT,
+    total_price DECIMAL,
+    invoice_id INT,
+    treament_id INT,
+    CONSTRAINT fk_invoices FOREIGN KEY(invoice_id) REFERENCES invoices(id),
     CONSTRAINT fk_treament FOREIGN KEY(treament_id) REFERENCES treaments(id)
 );
