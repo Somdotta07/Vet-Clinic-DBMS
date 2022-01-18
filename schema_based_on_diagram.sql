47d4e7dedd3a4b188c8f70a2408a03a999a9743e
Somdotta07 <jhilick.s@gmail.com>
2022-01-18 14:14:02 +0100

    add patients & medical history tables

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,13 @@
+CREATE TABLE patients (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(220),
+    date_of_birth DATE
+);
+
+CREATE TABLE medical_histories (
+    id SERIAL PRIMARY KEY,
+    admitted_at TIMESTAMP,
+   patient_id INT,
+   status VARCHAR(220),
+   CONSTRAINT fk_patient FOREIGN KEY(patient_id) REFERENCES patients(id)  
+);
